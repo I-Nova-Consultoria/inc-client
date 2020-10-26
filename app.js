db889c22372a4c1e0f6e29e66d02e4d67ce528ee
Lucas Passos <lucas.depassos@yahoo.com.br>
2020-10-26 14:08:47 -0300

    first_commit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const sign_in_btn = document.querySelector("#sign-in-btn");
-const sign_up_btn = document.querySelector("#sign-up-btn");
+const sign_up_btn = document.querySelector("#btnSolid");
 const container = document.querySelector(".container");
 
 sign_up_btn.addEventListener("click", () => {
@@ -9,3 +9,22 @@ sign_up_btn.addEventListener("click", () => {
 sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
 });
+
+function Alertar() {
+  confirm("Você está prestes a fazer uma alteração no banco de dados do aprova-fácil, deseja prosseguir?")
+}
+
+
+
+var valorVenda = document.getElementById('valor1')
+var proSolut = document.getElementById('solut1')
+
+
+function Calcular() {
+
+const Result = parseFloat(valorVenda) + (parseFloat(proSolut) / 100) * parseFloat(valorVenda) || 0
+
+document.getElementById('resultado1').innerHTML(Result)
+
+
+}
